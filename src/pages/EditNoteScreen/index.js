7b2f8903614b7adc82ce06a271512022de5e5fe0
Luiz Pires <lpiresjunior@gmail.com>
2021-01-18 22:26:05 -0300

    edit screen styles

--- src/pages/EditNoteScreen/index.js
+++ src/pages/EditNoteScreen/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import {Container, SimpleText} from './styles';
+import React, {useState} from 'react';
+import {Container, TitleInput, BodyInput} from './styles';
 
 export default () => {
+  const [title, setTitle] = useState();
+  const [body, setBody] = useState();
+
   return (
     <Container>
-      <SimpleText>Edit</SimpleText>
+      <TitleInput
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+        placeholder="Título"
+        placeholderTextColor="#CCC"
+      />
+      <BodyInput
+        value={body}
+        onChangeText={(text) => setBody(text)}
+        placeholder="... "
+        placeholderTextColor="#CCC"
+        multiline={true}
+        textAlignVertical="top"
+        autoFocus={true}
+      />
     </Container>
   );
 };
