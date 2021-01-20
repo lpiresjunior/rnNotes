54da63102ce185ba8587158ae52421f341e8829e
Luiz Pires <lpiresjunior@gmail.com>
2021-01-19 21:55:55 -0300

    send note to edit note screen

--- src/pages/EditNoteScreen/index.js
+++ src/pages/EditNoteScreen/index.js
@@ -1,9 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useLayoutEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {useNavigation, useRoute} from '@react-navigation/native';
+
 import {Container, TitleInput, BodyInput} from './styles';
 
 export default () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const dispatch = useDispatch();
+  const list = useSelector((state) => state.notes.list);
+
   const [title, setTitle] = useState();
   const [body, setBody] = useState();
+  const [status, setStatus] = useState('new');
+
+  useEffect(() => {
+    if (route.params?.NoteIndex !== undefined && list[route.params.NoteIndex]) {
+      setStatus('edit');
+      setTitle(list[route.params?.NoteIndex].title);
+      setBody(list[route.params?.NoteIndex].body);
+    }
+  }, []);
 
   return (
     <Container>
